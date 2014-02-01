acffedc1feb5d60760aeefa33d3b836491bc3cdc
lvercelli <lvercelli.jasoftsolutions@gmail.com>
2014-02-01 18:21:11 -0500

    Incluidas dependencias el proyecto symfony. Signed-off-by:lvercelli <lvercelli.jasoftsolutions@gmail.com>

--- agenda-backend/vendor/autoload.php
+++ agenda-backend/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd73f1ce3c8cd7da716f96303e06d0c95::getLoader();
+return ComposerAutoloaderInit9eeb833fe3478780882262404971a8aa::getLoader();
